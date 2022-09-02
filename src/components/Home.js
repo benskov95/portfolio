45e57b26106f8e66bc3fc9378c37cc3a40cc879b
Benjamin S <ben.sommer.skovgaard@gmail.com>
2022-09-02 23:20:12 +0200

    bubbles

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,15 @@ export default function Home() {
     return (
         <div id="home-container">
             <h1 id="home-title">Hi, I'm Benjamin.</h1>
-            <div id="bubble">
-                <p>I'm a full stack developer with a passion for the process of bringing ideas to life.</p>
-            </div>
+            <p>I'm a full stack developer with a passion for the process of bringing ideas to life.</p>
+            <div className="bubble x1">React</div>
+            <div className="bubble x2">JavaScript</div>
+            <div className="bubble x3">TypeScript</div>
+            <div className="bubble x4">SQL</div>
+            <div className="bubble x5">Java</div>
+            <div className="bubble x6">C#</div>
+            <div className="bubble x7">Xamarin</div>
+            <div className="bubble x8">Vue</div>
         </div>
     )
 }
