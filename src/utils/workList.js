e047f824b03b733d8709546a38486183e79ae614
Benjamin S <ben.sommer.skovgaard@gmail.com>
2022-09-15 17:57:01 +0200

    changed wording in a few places on the slides

--- src/utils/workList.js
+++ src/utils/workList.js
@@ -9,19 +9,19 @@ const workList =
         image: messengerPic,
         link: "https://benskov.com/messenger/",
         title: "Messenger",
-        desc: "This is a heavily Discord-inspired and simple messenger app I made with React, with a Java REST API for the back-end. <br /><br /> You can use the app to send/receive friend requests and send direct messages to people on your friend list. <br /><br /> I also set it up to use Websockets for real-time communication, and with Electron to make it available as a desktop app. <br /><br /> <a href=\"https://drive.google.com/file/d/1rnD5d-5WpW7sqxpcGzzGetCvwSqpdU7o/view?usp=sharing\" target=\"_blank\" rel=\"noreferrer\">Link to download the Electron app installer if you want to try it as a desktop app</a>" 
+        desc: "This is a heavily Discord-inspired and simple messenger app I made with React (JavaScript), with a Java REST API for the back-end. <br /><br /> You can use the app to send/receive friend requests and send direct messages to people on your friend list. <br /><br /> I also set it up to use Websockets for real-time communication, and with Electron to make it available as a desktop app. <br /><br /> <a href=\"https://drive.google.com/file/d/1rnD5d-5WpW7sqxpcGzzGetCvwSqpdU7o/view?usp=sharing\" target=\"_blank\" rel=\"noreferrer\">Download the Electron app installer here if you want to try it as a desktop app</a>" 
     },
     {
         image: restaurantPic,
         link: "https://benskov.com/restaurant/",
         title: "Restaurant",
-        desc: "A generic restaurant website I made with Vue and TypeScript. I also set up a basic REST API in Node (with TypeScript as well) for the menu items."
+        desc: "A generic restaurant website I made with Vue (TypeScript). I also set up a basic Node REST API (with TypeScript as well) for the menu items."
     },
     {
         image: shopPic,
         link: "https://benskov.com/shop/",
         title: "Webshop",
-        desc: "This is an older project I made for an assignment while I was studying CS. It's a basic webshop made with React and Java for the back-end. <br /><br /> I also used two external APIs - one for the products, and one for price conversion from USD to DKK.  <br /><br />In the app you can add products to your cart, buy them, and request refunds on completed orders (which are manually handled by an admin user)."
+        desc: "This is an older project I made for an assignment while I was studying CS. It's a basic webshop made with React (JavaScript), and Java for the back-end REST API. <br /><br /> I also used two external APIs - one for the products, and one for currency conversion from USD to DKK.  <br /><br />In the app you can add products to your cart, buy them, and request refunds on completed orders (which are manually handled by an admin user)."
     },
     {
         image: rpgPic,
